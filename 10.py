d2031c96b06df8f444730a0fc78eedad3f04b866
Rohith Gadde <gadderohith@gmail.com>
2023-09-05 11:38:22 -0400

    modified:   10.py

--- 10.py
+++ 10.py
@@ -1,8 +1,20 @@
 def is_leap(year):
-    leap = False
-    if year%100 == 0 and year%400 == 0:
-        leap == True
-    
-    return leap
 
-year = int(input())
+    # variable to check the leap year
+    leap = False
+
+    if (year % 400 == 0) and (year % 100 == 0):
+
+       # change leap to True
+        leap = True
+
+    elif (year % 4 == 0) and (year % 100 != 0):
+
+        # Change leap to true
+        leap = True
+
+    # else not a leap year
+    else:
+        pass
+
+    return leap
